2a03922d26775b8588ce2af0b4bf48f1c9019da0
vynguyen-bereadytechnology <vy.nguyen@bereadytechnology.com.au>
2025-09-05 19:34:33 +0700

    Update UserPolicy.php

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\User;
 use App\Policies\Traits\HandlesFilamentPermissions;
 
 class UserPolicy
@@ -16,4 +17,32 @@ class UserPolicy
 
     use HandlesFilamentPermissions;
     protected string $entity = 'user';
+
+    public function update(User $user, User $model): bool
+    {
+        if ($model->hasRole('Super_Admin') && !$user->hasRole('Super_Admin')) {
+            return false;
+        }
+
+        return $this->can($user, 'update');
+    }
+
+    public function delete(User $user, User $model): bool
+    {
+        if ($model->hasRole('Super_Admin') && !$user->hasRole('Super_Admin')) {
+            return false;
+        }
+
+        return $this->can($user, 'delete');
+    }
+
+    public function deleteAny(User $user): bool
+    {
+        if (!$user->hasRole('Super_Admin')) {
+            return false;
+        }
+
+        return $this->can($user, 'delete');
+    }
+
 }
